a922a7df5573a34499792052c1ad1931041f3d9b
nguyen2705 <minhvu0312@gmail.com>
2024-11-02 23:06:32 -0500

    Add more 3d objects and implementing the virtural environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls, useGLTF, useAnimations } from '@react-three/drei';
 
 function Model({ path, scale = [1, 1, 1], position = [0, 0, 0] }) {
-    const { scene } = useGLTF(path);
-    return <primitive object={scene} scale={scale} position={position} />;
+  const { scene, animations } = useGLTF(path);
+  const { actions } = useAnimations(animations, scene);
+
+  React.useEffect(() => {
+    if (actions) {
+      Object.values(actions).forEach((action) => action.play());
+    }
+  }, [actions]);
+
+  return <primitive object={scene} scale={scale} position={position} />;
 }
 
 function App() {
-    return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            <Canvas camera={{ position: [0, 2, -120], fov: 50 }}>
-                {/* Lights */}
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[5, 5, 5]} intensity={1} />
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas camera={{ position: [0, 2, 120], fov: 50 }}>
+        {/* Lights */}
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
 
-                {/* Background model - Larger scale and positioned further back */}
-                <Model path="/background.glb" scale={[200, 200, 200]} position={[0, 0, -5]} />
+        {/* Background model */}
+        <Model path="/background.glb" scale={[200, 200, 200]} position={[0, 0, -5]} />
 
-                {/* Pacman model - Positioned closer to the camera */}
-                <Model path="/pacman.glb" scale={[1, 1, 1]} position={[0, -40, 0]} />
+        {/* Pacman model */}
+        <Model path="/pacman.glb" scale={[1, 1, 1]} position={[0, -40, 0]} />
 
-                {/* OrbitControls to enable 360-degree rotation */}
-                <OrbitControls enableZoom={true} maxPolarAngle={Math.PI} minPolarAngle={0} />
-            </Canvas>
-        </div>
-    );
+        {/* Console model */}
+        <Model path="/console.glb" scale={[7, 7, 7]} position={[-60, -35, 50]} />
+
+        {/* OrbitControls to enable 360-degree rotation */}
+        <OrbitControls
+          enableZoom={true}
+          minDistance={50}
+          maxDistance={150}
+          maxPolarAngle={Math.PI}
+          minPolarAngle={0}
+        />
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
